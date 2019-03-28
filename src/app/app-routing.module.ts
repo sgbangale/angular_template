d548587ce73beefbf6ad7a809c836468eb87dd6b
Suraj <sgbangale@gmail.com>
2019-03-28 15:49:12 +0530

    removed access denied, blank page server error signup modules from application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,7 @@ import { AuthGuard } from './shared';
 
 const routes: Routes = [
     { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
-    { path: 'login', loadChildren: './login/login.module#LoginModule' },
-    { path: 'signup', loadChildren: './signup/signup.module#SignupModule' },
-    { path: 'error', loadChildren: './server-error/server-error.module#ServerErrorModule' },
-    { path: 'access-denied', loadChildren: './access-denied/access-denied.module#AccessDeniedModule' },
+    { path: 'login', loadChildren: './login/login.module#LoginModule' },    
     { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
     { path: '**', redirectTo: 'not-found' }
 ];
--- src/app/entities/entity-create/entity-create.component.ts
+++ src/app/entities/entity-create/entity-create.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/shared';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, Validators, FormGroup, FormArray, AbstractControl } from '@angular/forms';
-import { Observable, of } from 'rxjs';
-import { entityCreate, activeEntitiyDetails } from 'src/app/shared/requests/entity.request';
-import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ApiService } from 'src/app/shared';
+import { entityCreate } from 'src/app/shared/requests/entity.request';
 
 @Component({
   selector: 'app-entity-create',
@@ -12,51 +11,51 @@ import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
   styleUrls: ['./entity-create.component.scss']
 })
 export class EntityCreateComponent implements OnInit {
-
   constructor(private service: ApiService, private route: ActivatedRoute, private fb: FormBuilder) {
     this.formGroup = fb.group({
       entity_code: ['', Validators.required],
       entity_name: ['', Validators.required],
       entity_active: [true, Validators.required],
-      entity_schema : this.fb.array([ this.addGroup()])
+      entity_schema: this.fb.array([this.addGroup()])
     });
-   }
+  }
 
   entityData: any;
   entityName: Observable<any>;
   formGroup: FormGroup;
   public model: any;
-  isSubmit = false;
 
   columnTypes = ['String', 'Number', 'Date', 'Boolean', 'Mixed'];
 
-  get f() { return this.formGroup.controls; }
+  get f() {
+    return this.formGroup.controls;
+  }
 
   hasError(control: AbstractControl) {
-    return (control.dirty || control.touched)  && control.errors && this.isSubmit;
+    return (control.dirty || control.touched) && control.errors;
   }
+ 
+
 
   saveEntity() {
     if (this.formGroup.valid) {
-      this.isSubmit = true;
       entityCreate.request_data = this.formGroup.value;
       const schema = entityCreate.request_data.entity_schema as Array<any>;
       const db = {};
-        schema.forEach((v) => {
-        db[v.column_name] = { type : v.column_type};
-        });
+      schema.forEach(v => {
+        db[v.column_name] = { type: v.column_type };
+      });
 
-        entityCreate.request_data.entity_schema = db;
-        this.service.request('entity__create', entityCreate).subscribe(data => {
-          console.log(data.value.body);
-        });
+      entityCreate.request_data.entity_schema = db;
+      this.service.request('entity__create', entityCreate).subscribe(data => {
+        console.log(data.value.body);
+      });
     }
   }
   addGroup() {
     return this.fb.group({
       column_name: ['', Validators.required],
       column_type: ['', Validators.required]
-
     });
   }
 
@@ -64,11 +63,9 @@ export class EntityCreateComponent implements OnInit {
     (this.formGroup.get('entity_schema') as FormArray).push(this.addGroup());
   }
 
-
   removeColumn(index: any) {
     (this.formGroup.get('entity_schema') as FormArray).removeAt(index);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
